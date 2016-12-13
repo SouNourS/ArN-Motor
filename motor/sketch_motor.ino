9a417599b010b99f00f5b72e7271b88418a2ba6f
Xavier CASSOUDESALLE <xavier.cassoudesalle@onera.fr>
2016-12-13 11:40:28 +0100

    Ajout interface HTML

--- motor/sketch_motor.ino
+++ motor/sketch_motor.ino
@@ -36,16 +36,161 @@ void loop()
     //boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
-              //Envoi du header standard HTTP au browser
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-Type: text/html");
-              client.println();
-              //envoi donnees
-              client.print("Coucou toto ");
-              monServo.writeMicroseconds(1400);
-              Serial.println("Envoi message");
-         client.stop();}
-    }}
+	      // et dans tout les cas on répond au client
+	      repondre(client);
+	      // on quitte le while
+	      break;
+              //monServo.writeMicroseconds(1400);
+	      }
+     }
+     client.stop();
+    }
+void repondre(EthernetClient client) {
+	//Envoi du header standard HTTP au browser
+	client.println("HTTP/1.1 200 OK");
+	client.println("Content-Type: text/html");
+	client.println();
+	client.println("<HTML>");
+	client.println("<HEAD>");
+	client.println("<TITLE>Contrôle Caméra 1</TITLE>");
+	client.println("</HEAD>");
+	client.print("<body BGCOLOR="),client.print(byte(34)); // <body BGCOLOR="
+	client.print("#E0E0F8"); // #E0E0F8
+	client.print(byte(34)),client.print(">"); // " >
+	client.println("<center>");
+	client.println("<H1>Contrôle Caméra 1</H1>");
+	client.println("<br><br>");
+	// Début tableau caméras
+	client.println("<TABLE BORDER=0 WIDTH=80%>");
+	client.println("<TR>");
+	// IP 1
+	client.print("<TD ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><a href="),client.print(byte(34));
+	client.print("http://192.168.0.101"),client.print(byte(34));
+	client.print("><input type="),client.print(byte(34));
+	client.print("button"),client.print(byte(34));
+	client.print("value="),client.print(byte(34));
+	client.print("Caméra 1"),client.print(byte(34));
+	client.print("></a></TD>");
+	// IP 2
+	client.print("<TD ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><a href="),client.print(byte(34));
+	client.print("http://192.168.0.102"),client.print(byte(34));
+	client.print("><input type="),client.print(byte(34));
+	client.print("button"),client.print(byte(34));
+	client.print("value="),client.print(byte(34));
+	client.print("Caméra 2"),client.print(byte(34));
+	client.print("></a></TD>");
+	// IP 3
+	client.print("<TD ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><a href="),client.print(byte(34));
+	client.print("http://192.168.0.103"),client.print(byte(34));
+	client.print("><input type="),client.print(byte(34));
+	client.print("button"),client.print(byte(34));
+	client.print("value="),client.print(byte(34));
+	client.print("Caméra 3"),client.print(byte(34));
+	client.print("></a></TD>");
+	// IP 4
+	client.print("<TD ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><a href="),client.print(byte(34));
+	client.print("http://192.168.0.104"),client.print(byte(34));
+	client.print("><input type="),client.print(byte(34));
+	client.print("button"),client.print(byte(34));
+	client.print("value="),client.print(byte(34));
+	client.print("Caméra 4"),client.print(byte(34));
+	client.print("></a></TD>");
+	client.println("</TR>");
+	// Fin tableau caméras
+	client.println("</TABLE>");
+	client.println("<br>"); // saut de ligne
+	client.println("<br>"); // saut de ligne
+	// Début tableau Vitesse
+	client.println("<TABLE BORDER=0 WIDTH=50%>");
+	client.println("<tr>");	
+	// Vitesse lente
+	client.print("<td ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><INPUT TYPE="),client.print(byte(34));
+	client.print("radio"),client.print(byte(34));
+	client.print("NAME="),client.print(byte(34));
+	client.print("vitesse"),client.print(byte(34));
+	client.print("VALUE="),client.print(byte(34));
+	client.print("1"),client.print(byte(34));
+	client.print("CHECKED>Vitesse Lente</td>");
+	// Vitesse rapide
+	client.print("<td ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><INPUT TYPE="),client.print(byte(34));
+	client.print("radio"),client.print(byte(34));
+	client.print("NAME="),client.print(byte(34));
+	client.print("vitesse"),client.print(byte(34));
+	client.print("VALUE="),client.print(byte(34));
+	client.print("2"),client.print(byte(34));
+	client.print("CHECKED>Vitesse Rapide</td>");	
+	client.println("</tr>");
+	// Fin tableau Vitesse
+	client.println("</TABLE>");
+	client.println("<br>"); // saut de ligne
+	// Debut Tableau FDC + Boutons
+	client.println("<TABLE BORDER=0 WIDTH=30%>");
+	// Debut ligne FDC
+	client.print("<tr height="),client.print(byte(34));
+	client.print("50"),client.print(byte(34));
+	client.print(">");
+	// FDC 1
+	client.print("<td ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><INPUT TYPE="),client.print(byte(34));
+	client.print("radio"),client.print(byte(34));
+	client.print("NAME="),client.print(byte(34));
+	client.print("Fin de course"),client.print(byte(34));
+	client.print("VALUE="),client.print(byte(34));
+	client.print("FDC1"),client.print(byte(34));
+	client.print("disabled >Max Zoom&nbsp;&nbsp;</td>");
+	// FDC 2 
+	client.print("<td ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><INPUT TYPE="),client.print(byte(34));
+	client.print("radio"),client.print(byte(34));
+	client.print("NAME="),client.print(byte(34));
+	client.print("Fin de course"),client.print(byte(34));
+	client.print("VALUE="),client.print(byte(34));
+	client.print("FDC2"),client.print(byte(34));
+	client.print("disabled >Min Zoom&nbsp;&nbsp;</td>");
+	// Fin ligne FDC
+	client.println("</tr>");
+	// Debut ligne boutons
+	client.println("<tr>");
+	// Bouton +
+	client.print("<td ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><input type="),client.print(byte(34));
+	client.print("button"),client.print(byte(34));
+	client.print("  value="),client.print(byte(34));
+	client.print("        +        "),client.print(byte(34));
+	client.print("></td>");
+	// Bouton -
+	client.print("<td ALIGN="),client.print(byte(34));
+	client.print("center"),client.print(byte(34));
+	client.print("><input type="),client.print(byte(34));
+	client.print("button"),client.print(byte(34));
+	client.print("  value="),client.print(byte(34));
+	client.print("        -        "),client.print(byte(34));
+	client.print("></td>");
+	// Fin ligne boutons
+	client.println("</tr>");
+	// Fin Tableau FDC + Boutons
+	client.println("</TABLE>");
+	// Fin de fichier
+	client.println("</center>");
+	client.println("</body>");
+	client.println("</HTML>");
+	client.println("</HTML>");
+}
   //if (!client) {
 	//exemple
     //}
